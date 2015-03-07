ad8c160344265bb7d5061b4587c46ee8c43d486e
Kawashima Daichi <daichi0.0daichi@gmail.com>
2015-03-07 15:06:12 +0900

    雛形完成

---
+++ iOS/Shift/Shift/FloorViewController.h
@@ -0,0 +1,13 @@
+//
+//  FloorViewController.h
+//  Shift
+//
+//  Created by 川島 大地 on 2015/03/07.
+//  Copyright (c) 2015年 川島 大地. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FloorViewController : UIViewController
+
+@end
