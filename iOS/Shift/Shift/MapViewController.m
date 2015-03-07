d1e47702bfffa0a0f520a34584e9845428d0c60a
Kawashima Daichi <daichi0.0daichi@gmail.com>
2015-03-07 19:08:14 +0900

    マップ表示してタップしたら遷移

--- iOS/Shift/Shift/MapViewController.m
+++ iOS/Shift/Shift/MapViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015年 川島 大地. All rights reserved.
 //
 
+/*
 #import "MapViewController.h"
 
 @interface MapViewController ()
@@ -25,7 +26,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -33,6 +33,50 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
+
+@end
+
 */
+
+#import "MapViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
+#import "SolidViewController.h"
+
+@implementation MapViewController {
+    GMSMapView *mapView_;
+}
+
+- (void)viewDidLoad {
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+    
+    // Create a GMSCameraPosition that tells the map to display the
+    // coordinate -33.86,151.20 at zoom level 6.
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:35.6656277
+                                                            longitude:139.730963
+                                                                 zoom:18];
+    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.myLocationEnabled = YES;
+    mapView_.delegate = self;
+    mapView_.myLocationEnabled = YES;
+    self.view = mapView_;
+    
+    // Creates a marker in the center of the map.
+    GMSMarker *marker = [[GMSMarker alloc] init];
+    marker.position = CLLocationCoordinate2DMake(35.6656277, 139.730963);
+    //marker.title = @"Sydney";
+    //marker.snippet = @"Australia";
+    marker.map = mapView_;
+}
+
+-(void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    
+}
+
+- (void) mapView:(GMSMapView *)mapView didTapAtCoordinate:(CLLocationCoordinate2D)coordinate {
+    NSLog(@"tap");
+    SolidViewController *solidViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"solid_view"];
+    [self.navigationController showViewController:solidViewController sender:nil];
+    //[self presentViewController:solidViewController animated:YES completion:nil];
+}
 
 @end
--- iOS/Shift/Shift/SolidViewController.m
+++ iOS/Shift/Shift/SolidViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     NSLog(@"SolidViewController");
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
 }
 
 - (void)didReceiveMemoryWarning {
