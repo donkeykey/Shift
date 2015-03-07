3c321fa51bc9cfa5083a2f4f3e1b0e0c81e0b998
Kawashima Daichi <daichi0.0daichi@gmail.com>
2015-03-08 01:51:00 +0900

    saishinban

--- iOS/Shift/Shift/MapViewController.m
+++ iOS/Shift/Shift/MapViewController.m
@@ -6,37 +6,6 @@
 //  Copyright (c) 2015年 川島 大地. All rights reserved.
 //
 
-/*
-#import "MapViewController.h"
-
-@interface MapViewController ()
-
-@end
-
-@implementation MapViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    NSLog(@"MapViewController");
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-
-@end
-
-*/
 
 #import "MapViewController.h"
 #import <GoogleMaps/GoogleMaps.h>
@@ -44,39 +13,50 @@
 
 @implementation MapViewController {
     GMSMapView *mapView_;
+    GMSMarker *marker[100];
 }
 
 - (void)viewDidLoad {
-    [self.navigationController setNavigationBarHidden:YES animated:NO];
-    
-    // Create a GMSCameraPosition that tells the map to display the
-    // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:35.6656277
-                                                            longitude:139.730963
-                                                                 zoom:18];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:MIDLAT
+                                                            longitude:MIDLON
+                                                                 zoom:17.8];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
     mapView_.delegate = self;
     mapView_.myLocationEnabled = YES;
     self.view = mapView_;
     
-    // Creates a marker in the center of the map.
-    GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(35.6656277, 139.730963);
-    //marker.title = @"Sydney";
-    //marker.snippet = @"Australia";
-    marker.map = mapView_;
+    [self protMakerAtRandom];
 }
 
--(void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    
+- (void) protMakerAtRandom {
+    for (int i = 0; i < 20; i++) {
+        float weight = 600;
+        float randLat = arc4random_uniform(1000000)/(weight*1000000.0) - 1/(weight*2);
+        float randLon = arc4random_uniform(1000000)/(weight*1000000.0) - 1/(weight*2);
+        // Creates a marker in the center of the map.
+        marker[i] = [[GMSMarker alloc] init];
+        marker[i].position = CLLocationCoordinate2DMake(MIDLAT + randLat, MIDLON + randLon);
+        //marker.title = @"Sydney";
+        //marker.snippet = @"Australia";
+        marker[i].map = mapView_;
+    }
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
 }
 
 - (void) mapView:(GMSMapView *)mapView didTapAtCoordinate:(CLLocationCoordinate2D)coordinate {
     NSLog(@"tap");
     SolidViewController *solidViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"solid_view"];
     [self.navigationController showViewController:solidViewController sender:nil];
-    //[self presentViewController:solidViewController animated:YES completion:nil];
+}
+
+- (BOOL) mapView:(GMSMapView *)mapView didTapMarker:(GMSMarker *)marker {
+    SolidViewController *solidViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"solid_view"];
+    [self.navigationController showViewController:solidViewController sender:nil];
+    return true;
 }
 
 @end
